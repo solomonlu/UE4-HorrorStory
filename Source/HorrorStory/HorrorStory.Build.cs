231698936c111eca4f7eedb0d27fb9f65e7d0511
solomonlu <52169479@qq.com>
2017-04-14 17:18:50 +0800

    add fog of war

--- Source/HorrorStory/HorrorStory.Build.cs
+++ Source/HorrorStory/HorrorStory.Build.cs
@@ -6,7 +6,7 @@ public class HorrorStory : ModuleRules
 {
 	public HorrorStory(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RHI", "RenderCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
