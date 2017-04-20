f9a8f17947fcc555a4d8e354895d5a8b9925bc80
solomonlu <52169479@qq.com>
2017-04-20 09:20:00 +0800

    fix compile bug

--- Source/HorrorStory/FogOfWarManager.cpp
+++ Source/HorrorStory/FogOfWarManager.cpp
@@ -43,9 +43,11 @@ void AFogOfWarManager::BeginPlay() {
 void AFogOfWarManager::Tick(float DeltaSeconds) {
 	Super::Tick(DeltaSeconds);
 	if (FOWTexture && LastFOWTexture && bHasFOWTextureUpdate && bIsDoneBlending) {
-		LastFOWTexture->UpdateResourceW();
+		//LastFOWTexture->UpdateResourceW();
+		LastFOWTexture->UpdateResource();
 		UpdateTextureRegions(LastFOWTexture, (int32)0, (uint32)1, textureRegions, (uint32)(4 * TextureSize), (uint32)4, (uint8*)LastFrameTextureData.GetData(), false);
-		FOWTexture->UpdateResourceW();
+		//FOWTexture->UpdateResourceW();
+		FOWTexture->UpdateResource();
 		UpdateTextureRegions(FOWTexture, (int32)0, (uint32)1, textureRegions, (uint32)(4 * TextureSize), (uint32)4, (uint8*)TextureData.GetData(), false);
 		bHasFOWTextureUpdate = false;
 		bIsDoneBlending = false;
